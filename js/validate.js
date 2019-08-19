c950ac7243d52d6b530710ddcb26722332534c6a
Shamsul Bahrin <sbahrin3@gmail.com>
2019-08-19 22:20:35 +0800

    First Commits!

---
+++ js/validate.js
@@ -0,0 +1,46 @@
+function validateData() {
+	
+	var theForm = document.myform;
+	var count = theForm.elements.length;
+	var is_validated = true;
+	var messages = [];
+	for ( var i=0; i < count; i++) {
+		var e = theForm.elements[i];
+		if ( e.type != "button" ) {
+			if ( e.getAttribute("data-required") == "true") {
+				if ( e.value == "" ) {
+					is_validated = false;
+					e.style.border = "2px solid #ff0000";
+					if ( e.getAttribute("data-msg")) {
+						messages.push(e.getAttribute("data-msg"));
+					}
+				} else {
+					e.style.border = "1px solid #cccccc";
+					if ( e.getAttribute("data-pattern") ) {
+						var result = new RegExp(e.getAttribute("data-pattern")).test(e.value);
+						if ( result == false ) {
+							is_validated = false;
+							e.style.border = "2px solid #ff0000";
+							if ( e.getAttribute("data-msg")) {
+								messages.push(e.getAttribute("data-msg"));
+							}
+						}
+					}
+					
+				}
+				
+			}
+		}
+	}
+	
+	if ( is_validated ) {
+		//you may submit data here
+		$.jGrowl("Fields validation are all clear!", { position:'top-right', theme:'notify' });
+		
+	} else {
+		messages.forEach(function(msg) {
+			$.jGrowl(msg, { position:'top-right', theme:'warning' });
+		});
+	}
+	
+}
